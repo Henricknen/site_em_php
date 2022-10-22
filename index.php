8126adab40af0b0abd88cde7d34a9343ebd4b214
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-22 17:55:50 -0300

    inicio_da_criacao_do_site_em_'php'

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+    <link rel="stylesheet" href="css/stylo.css">
+</head>
+<body>
+    <p>Site</p>
+    <p>em PHP</p>
+    
+</body>
+</html>
