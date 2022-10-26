ac10c8811dc8b7ba482a143a19c4d24b74acbfb0
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-26 15:20:44 -0300

    ultilizando_funcao_do_php_'isset'_para_verificar_se_'page'_ja_foi_setada

--- conteudo.php
+++ conteudo.php
@@ -1,6 +1,7 @@
 <?php
 
-@$pagina = $_GET['page'];
+// @$pagina = $_GET['page'];
+$pagina = isset($_GET['page']) ? $_GET['page'] : "";        /* verificando se 'page' esta setada "SET" */
 
 if($pagina == 2) {
     require_once('mais.php');
