8d6b008a5538af01c7f309ed336b3bb81a7510ff
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-30 17:44:41 -0300

    criacao_da_variavel_''_que_recebera_os_dados_das_variaveis_e_que_sera_passada_para_a_função_'mysqli_query'_que_cadastrara_na_tabela_do_banco_de_dados

--- cadastrar.php
+++ cadastrar.php
@@ -19,10 +19,17 @@
     echo "Email".$email."<br>";
     echo "Senha".$senha."<br>";
     echo "Dica".$dica."<br>";
+
     echo "Capa".$capa."<br>";
     echo "tipo de arquivo da capa: ".$capa_tipo."<br>";
+
     echo "Foto de perfil".$perfil."<br>";
     echo "Tipo de arquivo da foto de perfil: ".$perfil_tipo."<br>";
 
+    $sql = "insert into tb_site(nome,email,senha,dica,perfil,capa) values
+('$nome','$email','$senha','$dica','$perfil','$capa');";
+mysqli_query($link,$sql);     // para cadastrar este dados no banco de dados 'bd_site' dentro da tabela 'tb_site' usa a função 'mysqli_query'
+
+echo "<a href = 'cadastro.php'>Cadastrar outro Cliente</a>";
 
 ?>
