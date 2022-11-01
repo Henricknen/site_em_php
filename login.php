36dae378357c2a71379ac2dbb5566e34dfd2fd57
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-01 15:56:33 -0300

    criando_sessao_ultilizando_'session'_e_verificando_suas_variaveis_com_'isset'

--- login.php
+++ login.php
@@ -3,8 +3,21 @@
 // arquivo de login
 require_once "conexao.php";     // fazendo a inclusão do arquivo de 'conexao.php'
 
-$login = $_POST["login"];
-$senha = $_POST["senha"];
+// iniciar 'sesssion'
+SESSION_START();
+
+$login = $_POST["login"];       // variavel '$login' recebe o que vem do campo que tem name 'login'
+$senha = $_POST["senha"];                // a variavel $senha recebe o que vem do campo name 'senha'
+
+// variaveis de sessão
+$_SESSION['login_user'] = $login;
+$_SESSION['senha_user'] = $senha;
+
+if(isset($_SESSION['login_user']) && isset($_SESSION['senha_user'])) {       // verificando se a variavel esta setada
+    echo "Sim";
+} else {
+    header('location:index.php');       // se tentarem acesso direto será redirecionado para 'index.php' 
+}
 
 echo "Login do Usuario: $login<br>";
 echo "Senha do usuario: $senha";
