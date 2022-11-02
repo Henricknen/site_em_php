f1e7d6eff12bc216a670c869d4452cdd295ad516
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-02 00:56:33 -0300

    verificando_se_usuario_esta_cadastrado_na_tabela_do_banco_de_dados_e_redirecionando-o_para_a_tela_de_usuario

--- login.php
+++ login.php
@@ -13,13 +13,23 @@ $senha = $_POST["senha"];                // a variavel $senha recebe o que vem d
 $_SESSION['login_user'] = $login;
 $_SESSION['senha_user'] = $senha;
 
-if(isset($_SESSION['login_user']) && isset($_SESSION['senha_user'])) {       // verificando se a variavel esta setada
-    echo "Sim";
+if(isset($_SESSION['login_user']) && isset($_SESSION['senha_user'])) {       // verificando se a variavel esta cadastrada no banco de dados
+    $sql =mysqli_query($link, "select * from tb_site WHERE email = '$login'");     // selecionando todos os campos da tabela 'tb_site' 
+    while($dados = mysqli_fetch_array($sql)) {      // 'mysqli_fetch_array' faz uma varredura dentro de um array
+        $email = $dados['email'];
+        $pass = $dados['senha'];
+    }
+
+    //inicio da verifição: se os dados batem com o que tem na tabela do banco de dados
+    if($login = $email && $senha == $pass) {
+        header('location: user.php');
+    } else {
+        header('location: index.php');
+    }
+    // fim da verificação
+
 } else {
     header('location:index.php');       // se tentarem acesso direto será redirecionado para 'index.php' 
 }
-
-echo "Login do Usuario: $login<br>";
-echo "Senha do usuario: $senha";
 
 ?>
