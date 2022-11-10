1136c87421dff9b369da80b7688a417d32e67099
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-09 21:39:26 -0300

    criando_arquivo_'postagens.php'_que_exibira_as_postagens_feitas_pelo_usuario_logado

--- conteudo.php
+++ conteudo.php
@@ -4,7 +4,7 @@
 $pagina = isset($_GET['page']) ? $_GET['page'] : "";        /* verificando se 'page' esta setada "SET" */
 
 if($pagina == 2) {
-    require_once 'mais.php';
+    require_once 'postagens.php';
 } else if ($pagina == 3) {
     require_once 'postagem.php';
 } else {
