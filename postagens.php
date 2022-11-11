96ef5a6cc59114b47529e08a490f66a34a5465b8
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-11 01:45:30 -0300

    criando_link_de_'upload'_das_postagens

--- postagens.php
+++ postagens.php
@@ -6,6 +6,8 @@
     $postagens = mysqli_query($link, $sql);        // o que está consulta será guadado em '$postagens' criada
     echo "<br>";
     while($dados = mysqli_fetch_array($postagens)) {        // fazendo uma varedura na variável '$postagens' e armazenando dentro de '$dados'
-        echo "<p class='posts'>$dados[id_postagem]: $dados[postagem]</p>";
+        echo "<p class='posts'>$dados[id_postagem]: $dados[postagem]
+        <a href= 'update.php?id_post = $dados[id_postagem]'>Atualizar postagens...</a>
+        </p>";
     }
 ?>
