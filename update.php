2aafd5ab4b092950616bacfb1be05669a1865721
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-12 17:06:05 -0300

    fazendo_o_texto_que_sera_atualizado_aparecer_no_campo_do_formulario_onde_sera_feita_a_alteracao

--- update.php
+++ update.php
@@ -6,12 +6,19 @@
     echo "id da postagem que será atualizada $id_update<br>";
     header('content-text/html; charset=utf-8');
 
+    require_once('conexao.php');        // pegar os dados do formulario de postagem
+    $sql  = "select * from tb_postagens where id_postagem = 'id_update'";       // fazebdo consulta na tabela 'tb_postagens' do banco de dados
+    $result = mysqli_query($link, $sql);     // '$link' é o link da conexão com o banco de dados e '$sql' é a variável que seleciona os dados da tabela do banco de dados
+    while($text_update = mysqli_fetch_array($result)) {
+        $text = $text_update['postagem'];
+    }
+
 ?>
 
         <form action="atualizar.php" method="post">
             <label>
                 Atualizar:<br><br>
-                <textarea name="atualiza" rows="10" cols="50" required></textarea><br><br>
+                <textarea name="atualiza" rows="10" cols="50" required><?php echo $text; ?></textarea><br><br>
                 <input type="hidden" name="id" value="<?php echo $id_update;?>">
                 <input type="submit" value="Atualizar">
             </label>
