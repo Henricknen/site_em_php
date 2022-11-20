83154a984da0dd789277cd66eb0e72dbc6bf585b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-19 21:15:17 -0300

    fazendo_a_atualizacao_do_ dados_provenientes_do_formulario_na_tabela_de_usuarios_do_banco_de_dados

--- atualizar_fotos.php
+++ atualizar_fotos.php
@@ -4,13 +4,11 @@
     SESSION_START();        // iniçiando sessão
     require_once 'verifica_login.php';      // verificando qual usuario está logado
 
-    echo "Arquivos de uploads de fotos para atualização<br>";
 
     $capa = $_FILES['capa'] ['name'];
     $perfil = $_FILES['perfil'] ['name'];
 
-    echo "Nome: $nome_log<br>";
-    echo "Foto da capa: $capa <br>";
-    echo "Foto de perfil: $perfil <br>";
+
+    mysqli_query($link, "update tb_site set capa = '$capa', perfil = '$perfil' where id_user = '$id_log'");      // fazendo um a atualização na 'tb_site' e setando os campos 'capa' e 'perfil'
 
 ?>
