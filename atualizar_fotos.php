a6566bc55c8894acfacfe3612a076674e81ee490
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-20 18:54:53 -0300

    fazendo_o_upload_das_fotos_na_atualizacao_mas_antes_fazendo_a_remocao_das_fotos_antigas_da_pasta_so_usuario_logado

--- atualizar_fotos.php
+++ atualizar_fotos.php
@@ -7,13 +7,26 @@
 
     $capa = $_FILES['capa'] ['name'];
     $perfil = $_FILES['perfil'] ['name'];
+    
+    $foto_perfil = "users/$email_log/$perfil_log";      // caminho da imagem que será atualizada dentro da variável '$foto_perfil'
+    $foto_capa = "users/$email_log/$capa_log";              // adiçionando o caminho da foto que será atualizada na variável '$foto_capa'
 
+    if($perfil_log != "perfil.png" && $capa_log != "capa.png") {      // fazendo uma verificação
+        unlink($foto_perfil);       // se existir foto gravadas na pasta serâo apagadas para depois fazer o upload
+        unlink($foto_capa);
+        
+        mysqli_query($link, "update tb_site set capa = '$capa', perfil = '$perfil' where id_user = '$id_log'");      // fazendo um a atualização na 'tb_site' e setando os campos 'capa' e 'perfil'
+        move_uploaded_file($_FILES['capa']['tmp_name'], "users/" .$email_log. "/" .$capa);       // função de pegar as imagens e colocar na pasta do usuario logado 
+        move_uploaded_file($_FILES['perfil']['tmp_name'], "users/" .$email_log. "/" .$perfil);
+        header('location: user.php');       // depois da atualização retornará para a página do usúario 'user.php'
+    } else {        // caso contrario será feito apenas o upload das fotos
+        mysqli_query($link, "update tb_site set capa = '$capa', perfil = '$perfil' where id_user = '$id_log'");      // fazendo um a atualização na 'tb_site' e setando os campos 'capa' e 'perfil'
+        move_uploaded_file($_FILES['capa']['tmp_name'], "users/" .$email_log. "/" .$capa);       // função de pegar as imagens e colocar na pasta do usuario logado 
+        move_uploaded_file($_FILES['perfil']['tmp_name'], "users/" .$email_log. "/" .$perfil);
+        header('location: user.php');       // depois da atualização retornará para a página do usúario 'user.php'
 
-    mysqli_query($link, "update tb_site set capa = '$capa', perfil = '$perfil' where id_user = '$id_log'");      // fazendo um a atualização na 'tb_site' e setando os campos 'capa' e 'perfil'
+    }
 
-    move_uploaded_file($_FILES['capa']['tmp_name'], "users/" .$email_log. "/" .$capa);       // função de pegar as imagens e colocar na pasta do usuario logado 
-    move_uploaded_file($_FILES['perfil']['tmp_name'], "users/" .$email_log. "/" .$perfil);       
-
-    header('location: user.php');       // depois da atualização retornará para a página do usúario 'user.php'
+    
 
 ?>
