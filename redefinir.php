66b529c91b9d9a21f38d2f82cc10ef01d085a9f7
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-21 12:15:04 -0300

    verificando_na_redefincao_de_senha_se_'email'_ja_existe

--- redefinir.php
+++ redefinir.php
@@ -5,7 +5,15 @@
     $email = $_POST['email'];
     $dica = $_POST['dica'];
 
-    echo "Email: $email <br> Dica: $dica <br>"
+    $sql = mysqli_query($link, "select * from tb_site where email = '$email'");        // fazendo vrificsç~so se email existe no banco de dados
+    while($dados =mysqli_fetch_array($sql)) {       // a variavel '$dados' funcionará como um array
+        $e = $dados['email'];
+    }
 
+    if($e == $email) {
+        echo "email existe";
+    } else {
+        echo "<a href='redefinr_senha.php'>Voltar a tela de refinição</a>";
+    }
 
 ?>
