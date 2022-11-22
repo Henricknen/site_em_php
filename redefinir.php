89b59e9fc43e6acfed85332456b73245a8592798
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-22 20:14:57 -0300

    criando_link_de_nova_senha

--- redefinir.php
+++ redefinir.php
@@ -5,15 +5,26 @@
     $email = $_POST['email'];
     $dica = $_POST['dica'];
 
-    $sql = mysqli_query($link, "select * from tb_site where email = '$email'");        // fazendo vrificsç~so se email existe no banco de dados
-    while($dados =mysqli_fetch_array($sql)) {       // a variavel '$dados' funcionará como um array
+
+    if($email != "") {        // criando uma verificação de variáveis prenchidas
+        // verificando 'select' se '$email' for diferente de vazio
+        $sql = mysqli_query($link, "select * from tb_site where email = '$email'");        // fazendo verificsção se email existe no banco de dados
+    while($dados = mysqli_fetch_array($sql)) {       // a variavel '$dados' funcionará como um array
         $e = $dados['email'];
+        $d = $dados['dica'];
+        $s = $dados['senha'];
+    }
+        // verificando se '$mail' e '$dica' são iguais aos dados cadastrados
+        if($e == $email && $dica == $d) {
+            echo "Senha: $s<br>";
+            echo "<a href='nova_senha.php'>Criar nova senha</a>";
+        } else {
+            echo "<a href='redefinir_senha.php'>Voltar a tela de refinição</a>";
+        }
+    } else {
+        header('location: redefinir_senha.php');
     }
 
-    if($e == $email) {
-        echo "email existe";
-    } else {
-        echo "<a href='redefinr_senha.php'>Voltar a tela de refinição</a>";
-    }
+
 
 ?>
