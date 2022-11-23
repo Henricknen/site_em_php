d1da7cad74b734524858cad532f45c5a26c5c0e2
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-11-22 21:02:59 -0300

    fazendo_melhorias_nas_verificacoes_e_adicionando_funcao_em_'redefinir.php'_para_saber_quantos_registros_estao_sendo_encontrados

--- redefinir.php
+++ redefinir.php
@@ -8,19 +8,30 @@
 
     if($email != "") {        // criando uma verificação de variáveis prenchidas
         // verificando 'select' se '$email' for diferente de vazio
-        $sql = mysqli_query($link, "select * from tb_site where email = '$email'");        // fazendo verificsção se email existe no banco de dados
+        $sql = mysqli_query($link, "select * from tb_site where email = '$email'");        // fazendo verificação se email existe no banco de dados
     while($dados = mysqli_fetch_array($sql)) {       // a variavel '$dados' funcionará como um array
         $e = $dados['email'];
         $d = $dados['dica'];
         $s = $dados['senha'];
     }
-        // verificando se '$mail' e '$dica' são iguais aos dados cadastrados
-        if($e == $email && $dica == $d) {
-            echo "Senha: $s<br>";
-            echo "<a href='nova_senha.php'>Criar nova senha</a>";
+
+        $contagem = mysqli_num_rows($sql);       // verificando a quantiade de linhas guardada dentro de '$sql'
+        echo "Quantidade de registros localizados: $contagem<br>";
+
+        if($contagem) {     // 1 true 2 true 3 true.... 0 false
+            echo "E-mail existe<br>";
+
+                   // verificando se '$mail' e '$dica' são iguais aos dados cadastrados
+            if($e == $email && $dica == $d) {
+                echo "Senha: $s<br>";
+                echo "<a href='nova_senha.php'>Criar nova senha</a>";
+            } else {
+                echo "<br><a href='redefinir_senha.php'>Existe algo errado volte a tela de refinição</a>";
+            }
+
         } else {
-            echo "<a href='redefinir_senha.php'>Voltar a tela de refinição</a>";
-        }
+            echo "Nada foi encontrado";
+        } 
     } else {
         header('location: redefinir_senha.php');
     }
